a202ac4637a04f6815adfb2001041e776df5cdad
andrew610-howhow <410530710@gms.tku.edu.tw>
2024-10-26 22:39:16 +0800

    123

---
+++ ts_wk/1024.js
@@ -0,0 +1,12 @@
+var a = 1;
+console.log(a);
+var b;
+b = 2;
+console.log(b);
+var c = 123;
+console.log(c);
+for (var i = 1; i < 10; i++) {
+    for (var j = 1; j < 10; j++) {
+        console.log("".concat(i, " x ").concat(j, "= ").concat(i * j));
+    }
+}
